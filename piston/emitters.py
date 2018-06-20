531da97280f6d88808de29df9ad897920f7567b0
SamCHogg <sam.c.hogg@gmail.com>
2018-06-20 12:56:55 +0100

    Replace DateTimeAwareJSONEncoder with DjangoJSONEncoder

--- piston/emitters.py
+++ piston/emitters.py
@@ -31,7 +31,7 @@ except ImportError:
 from django.utils.xmlutils import SimplerXMLGenerator
 from django.utils.encoding import smart_unicode
 from django.core.urlresolvers import reverse, NoReverseMatch
-from django.core.serializers.json import DateTimeAwareJSONEncoder
+from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse
 from django.core import serializers
 
@@ -411,7 +411,7 @@ class JSONEmitter(Emitter):
     """
     def render(self, request):
         cb = request.GET.get('callback', None)
-        seria = simplejson.dumps(self.construct(), cls=DateTimeAwareJSONEncoder, ensure_ascii=False, indent=4)
+        seria = simplejson.dumps(self.construct(), cls=DjangoJSONEncoder, ensure_ascii=False, indent=4)
 
         # Callback
         if cb and is_valid_jsonp_callback_value(cb):
